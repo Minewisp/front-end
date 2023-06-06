779da09f760140a7deae38f6298dd50f457c48cd
Kshitij Jande <kshitij.jande.main@gmail.com>
2023-06-06 16:53:25 +0530

    Working on Login and OTP, separated both components

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,19 +2,112 @@ import {
   Box,
   Button,
   Card,
+  Center,
   FormControl,
   FormLabel,
+  HStack,
   Heading,
   Image,
   Input,
+  PinInput,
+  PinInputField,
   Stack,
   StackDivider,
   Text,
   useColorModeValue,
 } from '@chakra-ui/react';
 import CopyableCode from '../utility/CopyableCode';
+import { useState } from 'react';
+
+const LoginStage = ({ username, setUsername, handleLogin }) => {
+  return (
+    <Stack divider={<StackDivider />} spacing={4}>
+      <Box textAlign="center" spacing={4}>
+        <Heading size="lg">Login</Heading>
+      </Box>
+      <Box textAlign="center">
+        <Text pt="2" fontSize="sm">
+          Join the server using the IP <CopyableCode code="minewisp.net" />.
+        </Text>
+        <Text pt="2" fontSize="sm">
+          Enter your Minecraft username below and click on the button when
+          ready.
+        </Text>
+      </Box>
+      <FormControl>
+        <Heading size="xs" textTransform="uppercase" mb={2}>
+          Username
+        </Heading>
+        <Input
+          type="text"
+          placeholder="Your Minecraft username"
+          value={username}
+          onChange={e => setUsername(e.target.value)}
+          required
+        />
+      </FormControl>
+      <Button
+        colorScheme="blue"
+        onClick={handleLogin}
+      >
+        Generate OTP
+      </Button>
+    </Stack>
+  );
+};
+
+const OtpStage = ({ otp, setOtp, handleOtpVerification }) => {
+  return (
+    <Stack divider={<StackDivider />} spacing={4}>
+      <Box textAlign="center" spacing={4}>
+        <Heading size="lg">Verification</Heading>
+      </Box>
+      <Box textAlign="center">
+        <Text pt="2" fontSize="sm">
+          Go back in-game and type the code you recieved.
+        </Text>
+      </Box>
+      <FormControl>
+        {/* <Heading size="xs" textTransform="uppercase" mb={2}>
+          One-Time Password
+        </Heading> */}
+        <Center>
+          <HStack>
+            <PinInput
+              size="lg"
+              autoFocus
+              value={otp}
+              onChange={v => setOtp(v)}
+              onComplete={handleOtpVerification}
+              isPassword={true}
+            >
+              <PinInputField />
+              <PinInputField />
+              <PinInputField />
+              <PinInputField />
+              <PinInputField />
+            </PinInput>
+          </HStack>
+        </Center>
+      </FormControl>
+      <Button colorScheme="blue" onClick={handleOtpVerification}>
+        Login
+      </Button>
+    </Stack>
+  );
+};
 
 export function Login() {
+  const [stage, setStage] = useState(1);
+  const [username, setUsername] = useState('');
+  const [otp, setOtp] = useState('');
+
+  const handleLogin = () => {
+    setTimeout(() => setStage(2), 1000); // just testing
+  };
+
+  const handleOtpVerification = () => {};
+
   return (
     <Box
       display="flex"
@@ -30,27 +123,23 @@ export function Login() {
         borderRadius={20}
         bg={useColorModeValue('gray.50', 'gray.700')}
       >
-        <Stack divider={<StackDivider />} spacing={4}>
-          <Box textAlign="center" spacing={4}>
-            <Box display="flex" justifyContent="center" mb="1.4rem">
-              <Image src="logo192.png" align="center" boxSize="5rem" />
-            </Box>
-            <Heading size="lg">Login</Heading>
-          </Box>
-          <Box textAlign="center">
-            <Text pt="2" fontSize="sm">
-              Join the server using the IP <CopyableCode code="minewisp.net" />.
-            </Text>
-            <Text pt="2" fontSize="sm">
-              Enter your Minecraft username below and click on Login when ready.
-            </Text>
-          </Box>
-          <FormControl>
-            <FormLabel>Minecraft Username</FormLabel>
-            <Input type="text" placeholder="Enter username" required />
-          </FormControl>
-          <Button colorScheme="blue">Generate OTP</Button>
-        </Stack>
+        <Box display="flex" justifyContent="center" mb="1.4rem">
+          <Image src="logo192.png" align="center" boxSize="5rem" />
+        </Box>
+        {stage === 1 && (
+          <LoginStage
+            username={username}
+            setUsername={setUsername}
+            handleLogin={handleLogin}
+          />
+        )}
+        {stage === 2 && (
+          <OtpStage
+            otp={otp}
+            setOtp={setOtp}
+            handleOtpVerification={handleOtpVerification}
+          />
+        )}
       </Card>
     </Box>
   );
